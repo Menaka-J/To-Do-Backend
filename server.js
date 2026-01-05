93ec0cd09863d11500455e684f5db38250cc84c4
Menaka J <menakaraman4@gmail.com>
2026-01-05 23:01:08 +0530

    c2

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const PORT = 3000;
 const app = express();
 
@@ -9,25 +10,65 @@ app.get("/", (req, res) => {
 });
 
 //creating item
-let todolist = [];
+// let todolist = [];
 
-app.post("/todos", (req, res) => {
+//connecting mongodb
+mongoose.connect('mongodb://localhost:27017/mern-app')
+    .then(() => {
+        console.log("MongoDB connected")
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+//creating schema
+const todoschema = new mongoose.Schema({
+    title: String,
+    description: String
+})
+
+const todomodel = mongoose.model("ToDo", todoschema);
+
+// app.post("/todos", async (req, res) => {
+//     const { title, description } = req.body;
+//     // const newitem = {
+//     //     id: todolist.length + 1,
+//     //     title,
+//     //     description
+//     // };
+//     // todolist.push(newitem);
+//     // console.log(newitem);
+//     try {
+//         const newtodo = new todomodel({ title, description });
+//         await newtodo.save();
+//         res.send(201).json({ message: "Item added ", data: newtodo });
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({ message: "Server error" });
+//     }
+// });
+
+// //getting item
+// app.get("/todos", (req, res) => {
+//     res.json(todolist);
+// });
+
+app.post("/todos", async (req, res) => {
     const { title, description } = req.body;
-    const newitem = {
-        id: todolist.length + 1,
-        title,
-        description
-    };
-    todolist.push(newitem);
-    console.log(newitem);
-    res.status(201).json(newitem);
-});
 
-//getting item
-app.get("/todos", (req, res) => {
-    res.json(todolist);
-});
+    try {
+        const newtodo = new todomodel({ title, description });
+        await newtodo.save();
 
+        res.status(201).json({
+            message: "Item added",
+            data: newtodo
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
 
 //starting the server
 app.listen(PORT, () => {
